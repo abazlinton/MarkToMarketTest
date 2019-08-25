d9a09e7c7ec406ca2bec6702880b621ddd49d8d0
Alex Bazlinton <abazlinton@gmail.com>
2019-08-25 13:12:43 +0100

    mimic branding a little

--- src/App.js
+++ src/App.js
@@ -13,27 +13,25 @@ mockFetches()
 function App() {
   return (
     <div className="App">
-      <Navbar expand="sm" variant="dark" bg="dark" fixed="top">
+      <Navbar expand="sm" variant="dark" bg="custom-black" fixed="top">
         <Nav
           activeKey="index"
         >
-          <Navbar.Brand href="/" className="bg-primary text-white px-3">
-            M2M
-            </Navbar.Brand>
+          <Navbar.Brand href="/" className="brand text-white px-3">M2M</Navbar.Brand>
           <Nav.Item>
             <LinkContainer to="/companies" exact={true} eventKey="companies-index">
 
-              <Nav.Link>Companies</Nav.Link>
+              <Nav.Link>COMPANIES</Nav.Link>
             </LinkContainer>
           </Nav.Item>
           <Nav.Item>
             <LinkContainer to="/projects" exact={true} eventKey="projects-index">
-              <Nav.Link>Projects</Nav.Link>
+              <Nav.Link>PROJECTS</Nav.Link>
             </LinkContainer>
           </Nav.Item>
           <Nav.Item>
             <LinkContainer to="/projects/new" exact={true} eventKey="projects-new">
-              <Nav.Link>New Project</Nav.Link>
+              <Nav.Link>NEW PROJECT</Nav.Link>
             </LinkContainer>
           </Nav.Item>
         </Nav>
